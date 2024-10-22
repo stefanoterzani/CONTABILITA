c1f009baa0872b7d81bfc1ed2c4a6026c24ad1e1
stefano <stefanoterzani51@gmail.com>
2024-10-22 17:13:47 +0200

    Tolte rem

--- app/home.js
+++ app/home.js
@@ -22,31 +22,3 @@ export default function Home() {
 
 
 
-/*
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { useAuth } from '../Context/AuthContext';  // Assicurati di avere il contesto di autenticazione
-import { auth } from '../firebaseConfig'; // Assicurati di importare la tua configurazione Firebase
-import { signOut } from 'firebase/auth';
-
-const Home = () => {
-  const { user } = useAuth();
-
-  const handleSignOut = async () => {
-    try {
-      await signOut(auth);  // Effettua il logout
-    } catch (error) {
-      console.error('Errore durante il logout:', error);
-    }
-  };
-
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Benvenuto, {user?.email || 'Utente'}</Text>
-      <Button title="Esci" onPress={handleSignOut} />
-    </View>
-  );
-};
-
-export default Home;
-*/
